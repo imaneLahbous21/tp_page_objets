eee8b190dff82c208cfe06015cbd5fdba2305227
ilahbous <imane.lahbous@outlook.fr>
2022-04-28 14:39:36 +0200

    first page objects

---
+++ page_object/HomePage.py
@@ -0,0 +1,18 @@
+class HomePage:
+    driver = 0
+    hamburger_menu_selector = "#nav-hamburger-menu"
+
+    # un constructeur
+    def __int__(self, driver):
+        self.driver = driver
+
+    def openAllMenu(self):
+        driver = webdriver.Chrome()
+
+
+
+    def openBookCategory(self):
+
+
+    def openAllBoks(self):
+
