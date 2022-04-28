07fe770ea60665ded8c869b8f0555d37bd48baf0
ilahbous <imane.lahbous@outlook.fr>
2022-04-28 10:16:00 +0200

    first commit

---
+++ test_amazon.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+
+
+def test_amazon1():
+    driver = webdriver.Chrome()
+    driver.get("https://www.amazon.fr/")
+    driver.maximize_window()
+    driver.quit()  #ferme le navigateur
